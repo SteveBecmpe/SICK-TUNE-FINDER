81cb10059d5207c4ff8f65acf396e6a7bdce5fa6
Steve Black <SteveBlackecmpe@gmail.com>
2021-01-16 11:58:42 -0500

    added wildcard to api ajax url to generate return object with list of anywhere it exists

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ function SearchArtist(artist) {
     // This is our API key. Add your own API key between the ""
     let APIKey = "&appid=523532";
     let ADBAPITOKEN = "CDCFB";
+    let wildCard = "%";
     // let NumOfDays = "&cnt=5";
     // let testURL = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=coldplay" + APIKey;
 
     let baseURL = "https://theaudiodb.com/api/v1/json/1/search.php?s=";
   
-    let searchURL = baseURL + artist;
+    let searchURL = baseURL + artist+wildCard;
   
 
     
